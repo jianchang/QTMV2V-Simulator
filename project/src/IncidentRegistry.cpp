0442d4c0b8649ad901dcf765788e7fad1e08a4c0
jianchang <changjian1985@gmail.com>
2012-07-19 15:17:34 -0400

    Initial commit

---
+++ project/src/IncidentRegistry.cpp
@@ -0,0 +1,18 @@
+/*
+ * File Name: IncidentRegistry.h
+ * Written by: Cong Liao
+ * Email: liaocong@seas.upenn.edu
+ */
+ 
+#include "IncidentRegistry.h"
+
+IncidentRegistry::IncidentRegistry()
+: m_mutexRegistry(true)
+{
+
+}
+
+IncidentRegistry::~IncidentRegistry()
+{
+	
+}
